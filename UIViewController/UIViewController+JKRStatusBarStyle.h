72ea26e7b69a681cf545a5da1b0cc721d7733a33
Joker <joker@JokerdeiMac.local>
2017-04-07 09:41:13 +0800

    fix name error

---
+++ UIViewController/UIViewController+JKRStatusBarStyle.h
@@ -0,0 +1,15 @@
+//
+//  UIViewController+JKRStatusBarStyle.h
+//  JKRSearchDemo
+//
+//  Created by Joker on 2017/4/7.
+//  Copyright © 2017年 Lucky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (JKRStatusBarStyle)
+
+@property (nonatomic, assign) BOOL jkr_lightStatusBar;
+
+@end
