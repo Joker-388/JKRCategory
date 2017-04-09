93ab1142e96a2295fbe5203165de9b63bb5b54d6
Joker <yanbing.hu@icloud.com>
2017-04-10 07:55:23 +0800

    update

---
+++ UIColor/UIColor+JKRColor.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+JKRColor.h
+//  JKRAddressSelect
+//
+//  Created by Lucky on 2017/4/9.
+//  Copyright © 2017年 Lucky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (JKRColor)
+
++ (UIColor *)jkr_colorWithHexString:(NSString *)hexString;
+
+@end
