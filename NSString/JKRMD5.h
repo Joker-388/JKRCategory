ee89a265333e1c4f6e3c4f449b2f842e1bfc3c87
Joker <joker@JokerdeiMac.local>
2017-04-24 12:17:35 +0800

    Add md5

---
+++ NSString/JKRMD5.h
@@ -0,0 +1,15 @@
+//
+//  JKRMD5.h
+//  JKRBaseProject
+//
+//  Created by Joker on 2017/4/24.
+//  Copyright © 2017年 Joker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JKRMD5 : NSString
+
+- (NSString *)jkr_md5String;
+
+@end
---
+++ NSString/JKRMD5.m
@@ -0,0 +1,33 @@
+//
+//  JKRMD5.m
+//  JKRBaseProject
+//
+//  Created by Joker on 2017/4/24.
+//  Copyright © 2017年 Joker. All rights reserved.
+//
+
+#import "JKRMD5.h"
+#import <CommonCrypto/CommonDigest.h>
+
+@implementation JKRMD5
+
+-(NSString *)jkr_md5String {
+    CC_MD5_CTX md5;
+    CC_MD5_Init (&md5);
+    CC_MD5_Update (&md5, [self UTF8String], (CC_LONG) [self length]);
+    
+    unsigned char digest[CC_MD5_DIGEST_LENGTH];
+    CC_MD5_Final (digest, &md5);
+    NSString *s = [NSString stringWithFormat: @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+                   digest[0],  digest[1],
+                   digest[2],  digest[3],
+                   digest[4],  digest[5],
+                   digest[6],  digest[7],
+                   digest[8],  digest[9],
+                   digest[10], digest[11],
+                   digest[12], digest[13],
+                   digest[14], digest[15]];
+    return s;
+}
+
+@end
