3e405364a3d0bb3ecc79d509950c455a725f3f43
Joker <joker@JokerdeiMac.local>
2017-04-24 16:15:18 +0800

    Add image compress

--- UIImage/UIImage+JKRImage.h
+++ UIImage/UIImage+JKRImage.h
@@ -10,7 +10,15 @@
 
 @interface UIImage (JKRImage)
 
+/// 通过颜色获取一张宽高都为1px的图片
 + (UIImage *)jkr_imageWithColor:(UIColor *)color;
+/// 通过颜色获取一张规定尺寸的图片
 + (UIImage *)jkr_imageWithColor:(UIColor *)color size:(CGSize)size;
+/// 将图片压缩到指定宽度
+- (UIImage *)jkr_compressWithWidth:(CGFloat)width;
+/// 将图片在子线程中压缩，block在主线层回调
+- (void)jkr_compressToDataLength:(NSInteger)length withBlock:(void(^)(UIImage *image))block;
+/// 将图片在当前线程中压缩
+- (UIImage *)jkr_compressToDataLength:(NSInteger)length;
 
 @end
--- UIImage/UIImage+JKRImage.m
+++ UIImage/UIImage+JKRImage.m
@@ -26,4 +26,58 @@
     return image;
 }
 
+- (UIImage *)jkr_compressWithWidth:(CGFloat)width {
+    if (width <= 0 || [self isKindOfClass:[NSNull class]] || self == nil) return nil;
+    CGSize newSize = CGSizeMake(width, width * (self.size.height / self.size.width));
+    UIGraphicsBeginImageContext(newSize);
+    [self drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
+}
+
+- (void)jkr_compressToDataLength:(NSInteger)length withBlock :(void (^)(UIImage *))block {
+    if (length <= 0 || [self isKindOfClass:[NSNull class]] || self == nil) block(nil);
+    __weak typeof(self) weakSelf = self;
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        __strong typeof(weakSelf) strongSelf = weakSelf;
+        UIImage *newImage = [strongSelf copy];
+        CGFloat scale = 1.0;
+        NSInteger newImageLength = UIImagePNGRepresentation(newImage).length;
+        while (newImageLength > length) {
+            // 如果限定的大小比当前的尺寸大0.9的平方倍，就用开方求缩放倍数,减少缩放次数
+            if ((double)length / (double)newImageLength < 0.81) {
+                scale = sqrtf((double)length / (double)newImageLength);
+            } else {
+                scale = 0.9;
+            }
+            CGFloat width = newImage.size.width * scale;
+            newImage = [newImage jkr_compressWithWidth:width];
+            newImageLength = UIImagePNGRepresentation(newImage).length;
+        }
+        dispatch_async(dispatch_get_main_queue(), ^{
+            block(newImage);
+        });
+    });
+}
+
+- (UIImage *)jkr_compressToDataLength:(NSInteger)length {
+    if (length <= 0 || [self isKindOfClass:[NSNull class]] || self == nil) return nil;
+    UIImage *newImage = [self copy];
+    CGFloat scale = 1.0;
+    NSInteger newImageLength = UIImagePNGRepresentation(newImage).length;
+    while (newImageLength > length) {
+        // 如果限定的大小比当前的尺寸大0.9的平方倍，就用开方求缩放倍数,减少缩放次数
+        if ((double)length / (double)newImageLength < 0.81) {
+            scale = sqrtf((double)length / (double)newImageLength);
+        } else {
+            scale = 0.9;
+        }
+        CGFloat width = newImage.size.width * scale;
+        newImage = [newImage jkr_compressWithWidth:width];
+        newImageLength = UIImagePNGRepresentation(newImage).length;
+    }
+    return newImage;
+}
+
 @end
