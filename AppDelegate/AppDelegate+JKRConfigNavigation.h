6c07b301da8f09004ef301fea2ab75c70b09d9d7
Lucky <lucky@LuckydeMacBook-Pro.local>
2017-05-10 11:41:57 +0800

    Add navigation config

---
+++ AppDelegate/AppDelegate+JKRConfigNavigation.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate+JKRConfigNavigation.h
+//  RongCloudDemo
+//
+//  Created by Lucky on 2017/5/4.
+//  Copyright © 2017年 KaiHei. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate (JKRConfigNavigation)
+
+- (void)jkr_configNavigation;
+
+@end
