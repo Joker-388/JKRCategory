ed19c7c59ee8b171f52ad4a0ac883e3e4f4c2709
Lucky <lucky@LuckydeMacBook-Pro.local>
2017-06-27 15:32:51 +0800

    update

---
+++ NSString/NSString+JKRSize.h
@@ -0,0 +1,19 @@
+//
+//  NSString+JKRSize.m
+//  JKRMVVMDemo
+//
+//  Created by Lucky on 2017/4/27.
+//  Copyright © 2017年 Lucky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSString (JKRSize)
+
+- (CGSize)sizeWithWidth:(CGFloat)width font:(CGFloat)font;
+
+@end
+
+NS_ASSUME_NONNULL_END
