0dad6c19d9710017b9fdd7ff90226cf64a785928
huyanbing <joker@JokerdeiMac.local>
2018-11-06 17:53:30 +0800

    update

--- Category/UIImage/UIImageView+JKRImagesAnimation.m
+++ Category/UIImage/UIImageView+JKRImagesAnimation.m
@@ -7,40 +7,41 @@
 //
 
 #import "UIImageView+JKRImagesAnimation.h"
+#import <objc/runtime.h>
 
 @interface UIImageView ()
+
+@property (nonatomic, strong) dispatch_source_t timer;
 
 @end
 
 @implementation UIImageView (JKRImagesAnimation)
 
-static dispatch_source_t timer;
-
 - (void)jkr_startAnimationWithDuration:(NSTimeInterval)duration images:(NSArray *)images completion:(void (^)(BOOL))completion {
-    if (timer) [self jkr_finishAnimation];
+    if (self.timer) [self jkr_finishAnimation];
     CGFloat idx = duration / images.count;
     __block int i = 0;
-    timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_main_queue());
-    dispatch_source_set_timer(timer, DISPATCH_TIME_NOW, idx * NSEC_PER_SEC, 0 * NSEC_PER_SEC);
-    dispatch_source_set_event_handler(timer, ^{
+    self.timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_main_queue());
+    dispatch_source_set_timer(self.timer, DISPATCH_TIME_NOW, idx * NSEC_PER_SEC, 0 * NSEC_PER_SEC);
+    dispatch_source_set_event_handler(self.timer, ^{
         if (i >= images.count) {
-            dispatch_source_cancel(timer);
-            timer = nil;
+            dispatch_source_cancel(self.timer);
+            self.timer = nil;
             completion(YES);
         } else {
             self.image = images[i++];
         }
     });
-    dispatch_resume(timer);
+    dispatch_resume(self.timer);
 }
 
 - (void)jkr_startRepeatAnimationWithDuration:(NSTimeInterval)duration images:(NSArray *)images {
-    if (timer) [self jkr_finishAnimation];
+    if (self.timer) [self jkr_finishAnimation];
     CGFloat idx = duration / images.count;
     __block int i = 0;
-    timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_main_queue());
-    dispatch_source_set_timer(timer, DISPATCH_TIME_NOW, idx * NSEC_PER_SEC, 0 * NSEC_PER_SEC);
-    dispatch_source_set_event_handler(timer, ^{
+    self.timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_main_queue());
+    dispatch_source_set_timer(self.timer, DISPATCH_TIME_NOW, idx * NSEC_PER_SEC, 0 * NSEC_PER_SEC);
+    dispatch_source_set_event_handler(self.timer, ^{
         if (i >= images.count) {
             i = 0;
             self.image = images[i++];
@@ -48,15 +49,23 @@ static dispatch_source_t timer;
             self.image = images[i++];
         }
     });
-    dispatch_resume(timer);
+    dispatch_resume(self.timer);
 }
 
 - (void)jkr_finishAnimation {
-    if (timer) {
-        dispatch_source_cancel(timer);
-        timer = nil;
+    if (self.timer) {
+        dispatch_source_cancel(self.timer);
+        self.timer = nil;
         self.image = nil;
     }
+}
+
+- (void)setTimer:(dispatch_source_t)timer {
+    objc_setAssociatedObject(self, @selector(timer), timer, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (dispatch_source_t)timer {
+    return objc_getAssociatedObject(self, @selector(timer));
 }
 
 @end
