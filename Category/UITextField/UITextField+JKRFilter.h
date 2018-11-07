4f1c79fd039b1edc9b718c607bd4ccf3693c2d3f
huyanbing <joker@JokerdeiMac.local>
2018-11-07 09:31:21 +0800

    UITextField长度限制分类

--- Category/UITextField/UITextField+JKRFilter.h
+++ Category/UITextField/UITextField+JKRFilter.h
@@ -2,19 +2,27 @@
 //  UITextField+JKRFilter.h
 //  JKRCategoryDemo
 //
-//  Created by Joker on 2018/11/5.
-//  Copyright © 2018 Joker. All rights reserved.
+//  Created by Joker on 2017/10/5.
+//  Copyright © 2017 Joker. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
+typedef void(^jkr_textFieldChangeBlock)(UITextField *textField);
+
 @interface UITextField (JKRFilter)
 
-/// 限制输入多少位
+#pragma mark - delegate:shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+/// 限制输入多少位，中文或联想输入可能会被屏幕
 - (BOOL)jkr_limitInputMaxLength:(NSInteger)length shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string;
-
 /// 限制输入仅限数字
 - (BOOL)jkr_limitInputNumberWithShouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string;
+
+#pragma mark - ControlEvents 会和ControlEvents监听冲突
+/// 添加长度限制，支持联想和中文
+- (void)jkr_addlimitMaxInputLength:(NSInteger)length;
+/// 添加长度限制，支持联想和中文，回调实时文字改变
+- (void)jkr_addlimitMaxInputLength:(NSInteger)length changeBlock:(jkr_textFieldChangeBlock)changeBlock;
 
 @end
 
--- Category/UITextField/UITextField+JKRFilter.m
+++ Category/UITextField/UITextField+JKRFilter.m
@@ -2,11 +2,19 @@
 //  UITextField+JKRFilter.m
 //  JKRCategoryDemo
 //
-//  Created by Joker on 2018/11/5.
-//  Copyright © 2018 Joker. All rights reserved.
+//  Created by Joker on 2017/10/5.
+//  Copyright © 2017 Joker. All rights reserved.
 //
 
 #import "UITextField+JKRFilter.h"
+#import <objc/runtime.h>
+
+@interface UITextField ()
+
+@property (nonatomic, assign) NSInteger jkr_max_limit;
+@property (nonatomic, copy) jkr_textFieldChangeBlock jkr_textFieldChangeBlock;
+
+@end
 
 @implementation UITextField (JKRFilter)
 
@@ -37,6 +45,57 @@
         i++;
     }
     return res;
+}
+
+- (void)jkr_addlimitMaxInputLength:(NSInteger)length {
+    [self jkr_addlimitMaxInputLength:length changeBlock:nil];
+}
+
+- (void)jkr_addlimitMaxInputLength:(NSInteger)length changeBlock:(jkr_textFieldChangeBlock)changeBlock {
+    if (length <= 0) return;
+    self.jkr_max_limit = length;
+    self.jkr_textFieldChangeBlock = changeBlock;
+    [self addTarget:self action:@selector(textDidChange:) forControlEvents:UIControlEventEditingChanged];
+}
+
+- (NSInteger)jkr_max_limit {
+    return [objc_getAssociatedObject(self, @selector(jkr_max_limit)) integerValue];
+}
+
+- (void)setJkr_max_limit:(NSInteger)jkr_max_limit {
+    objc_setAssociatedObject(self, @selector(jkr_max_limit), [NSNumber numberWithInteger:jkr_max_limit], OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (jkr_textFieldChangeBlock)jkr_textFieldChangeBlock {
+    return objc_getAssociatedObject(self, @selector(jkr_textFieldChangeBlock));
+}
+
+- (void)setJkr_textFieldChangeBlock:(jkr_textFieldChangeBlock)jkr_textFieldChangeBlock {
+    objc_setAssociatedObject(self, @selector(jkr_textFieldChangeBlock), jkr_textFieldChangeBlock, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (void)textDidChange:(UITextField *)textField {
+    NSInteger kMaxLength = self.jkr_max_limit;
+    NSString *toBeString = textField.text;
+    NSString *lang = [[UIApplication sharedApplication]textInputMode].primaryLanguage; //ios7之前使用[UITextInputMode currentInputMode].primaryLanguage
+    if ([lang isEqualToString:@"zh-Hans"]) { //中文输入
+        UITextRange *selectedRange = [textField markedTextRange];
+        //获取高亮部分
+        UITextPosition *position = [textField positionFromPosition:selectedRange.start offset:0];
+        if (!position) {// 没有高亮选择的字，则对已输入的文字进行字数统计和限制
+            if (toBeString.length > kMaxLength) {
+                textField.text = [toBeString substringToIndex:kMaxLength];
+            }
+        } else {//有高亮选择的字符串，则暂不对文字进行统计和限制
+            
+        }
+    }else{//中文输入法以外的直接对其统计限制即可，不考虑其他语种情况
+        if (toBeString.length > kMaxLength) {
+            textField.text = [toBeString substringToIndex:kMaxLength];
+        }
+    }
+    
+    if (self.jkr_textFieldChangeBlock) self.jkr_textFieldChangeBlock(textField);
 }
 
 @end
